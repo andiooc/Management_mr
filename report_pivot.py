c46e31a8d3e0b2e1723d4c416f9f3a99b31a28ba
andiooc <547692699@qq.com>
2023-12-09 13:54:04 +0800

    2023-12-08 末级已完成，聚合无问题。接下来就将聚合参数放进来、以及把格式字段实现的过程了

--- report_pivot.py
+++ report_pivot.py
@@ -1,20 +1,15 @@
 import pandas as pd
-import output
+# import output
 def calculate_mr_level(row):
     report_id = row['管报ID']  # 获取行索引中的 '管报ID' 值
     if report_id.startswith('SC'):
-        return 0
+        mylevel = 0
     else:
-        return (len(report_id) - 2) / 2
-
+        mylevel = (len(report_id) - 2) / 2
+    return mylevel
 def reportpivot(df_dict):
     maindf = df_dict['maindf']
     dimensiondf = df_dict['dimensiondf']
-    # pivot_table = pd.crosstab(index=[maindf['管报ID'], maindf['父ID']],
-    #                           columns=maindf['国家名称'],
-    #                           values=maindf['值'],
-    #                           aggfunc='sum',
-    #                           dropna=False).reset_index()
     pivot_table=maindf.pivot_table(index=['管报ID', '父ID'], columns='国家名称', values='值', aggfunc='sum', fill_value=0).reset_index()
     pivot_table['mr_level'] = pivot_table.apply(calculate_mr_level, axis=1)
 
@@ -22,23 +17,24 @@ def reportpivot(df_dict):
     for i in range(max_level,0,-1):
         select_rows = pivot_table[pivot_table['mr_level'] == i]
         agg_data = select_rows.groupby('父ID').sum().reset_index().drop('管报ID', axis=1)
-        output.txtoutput(agg_data)
         agg_data.rename(columns={'父ID': '管报ID'}, inplace = True)
         agg_data['父ID'] = agg_data['管报ID'].str[:-2]
         agg_data['mr_level'] = i-1
         agg_data.reset_index(inplace=True)
         agg_data = agg_data[pivot_table.columns]
         pivot_table = pd.concat([pivot_table,agg_data], ignore_index= True)
+        print("当前的i等级：" + str(i) +"………………………………………………………………")
+        print(pivot_table[pivot_table['mr_level'] == 4.00])
 
 
     final_df = pd.merge(dimensiondf, pivot_table, left_on='mr_code', right_on='管报ID', how='left').fillna(0)
 
-    for index, row in final_df.iterrows():
-        if row['mr_level'] == max_level - 1:
-            parent_code = row['mr_code']
-            numeric_subset = final_df[final_df['mr_parentcode'] == parent_code].select_dtypes(include=['number'])
-            parent_sum = numeric_subset.sum()
-            final_df.loc[index, numeric_subset.columns] = parent_sum
+    # for index, row in final_df.iterrows():
+    #     if row['mr_level'] == max_level - 1:
+    #         parent_code = row['mr_code']
+    #         numeric_subset = final_df[final_df['mr_parentcode'] == parent_code].select_dtypes(include=['number'])
+    #         parent_sum = numeric_subset.sum()
+    #         final_df.loc[index, numeric_subset.columns] = parent_sum
 
 
     return final_df
